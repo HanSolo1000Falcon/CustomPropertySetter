3827a847411d72d83f3ee85082b3d9a22a7c8410
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-08-09 11:43:33 +0200

    Initial commit

---
+++ CustomPropertySetter/Constants.cs
@@ -0,0 +1,9 @@
+namespace CustomPropertySetter
+{
+    public class Constants
+    {
+        public const string PluginGuid = "hansolo1000falcon.custompropertysetter";
+        public const string PluginName = "CustomPropertySetter";
+        public const string PluginVersion = "1.0.0";
+    }
+}
---
+++ CustomPropertySetter/Plugin.cs
@@ -0,0 +1,63 @@
+﻿using BepInEx;
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using UnityEngine;
+
+namespace CustomPropertySetter
+{
+    [BepInPlugin(Constants.PluginGuid, Constants.PluginName, Constants.PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Rect windowRect = new Rect(100, 100, 200, 200);
+
+        private string prop1;
+        private string prop2;
+
+        private bool shouldBeOpen;
+        
+        private void Start()
+        {
+            Hashtable properties = new Hashtable();
+            properties.Add("Custom Property Setter", "Stop reading my custom props bruv");
+            PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
+        }
+
+        private void OnGUI()
+        {
+            if (!shouldBeOpen)
+                return;
+            
+            GUIStyle windowStyle = new GUIStyle(GUI.skin.window);
+            windowStyle.fontSize = 16;
+            windowStyle.alignment = TextAnchor.UpperCenter;
+            
+            windowRect.height = 0;
+            windowRect = GUILayout.Window(148, windowRect, DrawWindow, "Custom Property Setter", windowStyle);
+        }
+
+        private void DrawWindow(int id)
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Space(30);
+            
+            prop1 = GUILayout.TextField(prop1);
+            prop2 = GUILayout.TextField(prop2);
+            
+            if (GUILayout.Button("Set Custom Properties"))
+            {
+                Hashtable properties = new Hashtable();
+                properties.Add(prop1, prop2);
+                PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
+            }
+            
+            GUILayout.EndVertical();
+            GUI.DragWindow(new Rect(0, 0, windowRect.width, 25));
+        }
+
+        private void Update()
+        {
+            if (UnityInput.Current.GetKeyDown(KeyCode.F1))
+                shouldBeOpen = !shouldBeOpen;
+        }
+    }
+}
